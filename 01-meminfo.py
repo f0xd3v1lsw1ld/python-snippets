8887fb8bafa947c1b9246cc4910294897069f80e
f0xd3v1lsw1ld <f0xd3v1lsw1ld@gmail.com>
2016-02-07 18:42:29 +0100

    fix interpreter

--- 01-meminfo.py
+++ 01-meminfo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 __author__="f0xd3v1lsw1ld@gmail.com"
 
 import os
@@ -9,17 +9,17 @@ from time import gmtime, strftime
 
 def MemInfo():
  tempFile = open( "/proc/meminfo" )
- memfree = tempFile.read() 
+ memfree = tempFile.read()
  tempFile.close()
  stri=memfree.split('\n')
  memtotal=int(stri[0].replace("MemTotal:","").replace("kB","").replace(" ",""));
  memfree=int(stri[1].replace("MemFree:","").replace("kB","").replace(" ",""));
  memused=memtotal-memfree
  return (memtotal,memused,memfree)
- 
-def main(): 
-   print("KiB Mem %i total, %i used, %i free"%MemInfo())   
-    
+
+def main():
+   print("KiB Mem %i total, %i used, %i free"%MemInfo())
+
 
 
 if __name__=="__main__":
